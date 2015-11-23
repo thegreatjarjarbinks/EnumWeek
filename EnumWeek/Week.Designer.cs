4776de1901f50f43a7a244528607afa6825c9b9a
RyanStLouis <ryan.stlouis@ocsbstudent.ca>
2015-11-23 12:54:15 -0500

    EZPZ

---
+++ EnumWeek/Week.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EnumWeek
+{
+    partial class frmWeek
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDays = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstDays
+            // 
+            this.lstDays.FormattingEnabled = true;
+            this.lstDays.Location = new System.Drawing.Point(34, 59);
+            this.lstDays.Name = "lstDays";
+            this.lstDays.Size = new System.Drawing.Size(121, 121);
+            this.lstDays.TabIndex = 0;
+            this.lstDays.SelectedIndexChanged += new System.EventHandler(this.lstDays_SelectedIndexChanged);
+            // 
+            // frmWeek
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstDays);
+            this.Name = "frmWeek";
+            this.Text = "Week";
+            this.Load += new System.EventHandler(this.frmWeek_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDays;
+    }
+}
+
---
+++ EnumWeek/Week.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by: Alex Mathias
+ * Created on: 23-Nov-2015
+ * Created for: ICS3U
+ * Daily Assignment – Unit 6-01
+ * This program displays an Enum containing the days of the week.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumWeek
+{
+    public partial class frmWeek : Form
+    {
+        //Global Variables
+        enum WEEK
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        
+        
+        public frmWeek()
+        {
+            InitializeComponent();
+        }
+
+        private void frmWeek_Load(object sender, EventArgs e)
+        {
+            //Load the enum into the listbox
+            foreach (WEEK singleDay in Enum.GetValues(typeof(WEEK)))
+            {
+                this.lstDays.Items.Add(singleDay);
+            }
+        }
+
+        private void lstDays_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Saying what item the user selected
+            string selectValue;
+
+            selectValue = Convert.ToString(lstDays.SelectedItem);
+
+            MessageBox.Show("Your favourite day of the week is " + selectValue, "Enum Week");
+        }
+    }
+}
